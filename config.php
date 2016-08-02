406e93c3987ec4f6c42aea816f8834c2cd874c2a
Serkan Yildiz <genkstar76@gmail.com>
2016-08-02 14:08:37 +0200

    Config file

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+/**
+* @author Serkan Yildiz
+* @license MIT
+*/
+
+return [
+	/**
+	* URL to parse with GTFS-RT data
+	*/
+	'trip_updates_url' 		=> '',
+	/**
+	* URL to parse with Service Alerts data
+	*/
+	'service_alerts_url'	=> '',
+];
